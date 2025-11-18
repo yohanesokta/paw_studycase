0db40c7b46d7fc93f6b7638a2efdf7d4dba8736c
suryamaulana98 <suryamaulana757@gmail.com>
2025-11-18 23:17:23 +0700

    progress login

--- lib/loader.php
+++ lib/loader.php
@@ -1,5 +1,6 @@
-<?php 
+<?php
 require_once "lib/controllers.php";
 require_once "config/koneksi.php";
 require_once "controllers/main_controller.php";
+require_once "controllers/authController.php";
 require_once "controllers/google_controller.php";
--- routes/routes.php
+++ routes/routes.php
@@ -2,9 +2,13 @@
 require_once  "lib/routes.php";
 $Route = new Routes();
 
-$Route->GET("/" , "MainController@index");
+$Route->GET("/", "MainController@index");
 
-$Route->GET("/auth/google","GoogleController@redirect");
-$Route->GET("/auth/google/callback","GoogleController@callback");
+$Route->GET("/auth/google", "GoogleController@redirect");
+$Route->GET("/auth/google/callback", "GoogleController@callback");
+
+
+$Route->GET("/google", "authController@login");
+$Route->GET("/google/callback", "authController@callback");
 
 $Route->JalankanRouting();
