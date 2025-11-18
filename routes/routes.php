9803932e54dc6d2d65c5e5bdc480bdde0dcb1384
suryamaulana98 <suryamaulana757@gmail.com>
2025-11-18 23:55:16 +0700

    delete file googleController

--- routes/routes.php
+++ routes/routes.php
@@ -4,10 +4,6 @@ $Route = new Routes();
 
 $Route->GET("/", "MainController@index");
 
-$Route->GET("/auth/google", "GoogleController@redirect");
-$Route->GET("/auth/google/callback", "GoogleController@callback");
-
-
 $Route->GET("/google", "authController@login");
 $Route->GET("/google/callback", "authController@callback");
 
