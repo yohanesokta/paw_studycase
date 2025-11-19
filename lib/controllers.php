8589cd943687db312bc865bdb79a3751e657e7b6
yohanesokta <yohanesoktayt72@gmail.com>
2025-11-19 18:08:42 +0700

    feat  : middleware routing

--- lib/controllers.php
+++ lib/controllers.php
@@ -1,4 +1,12 @@
 <?php
+function URL($url) {
+    return $_ENV['proyekname'].$url;
+}
+function Redirect($url) {
+    header("Location: ".URL($url));
+    exit;
+}
+
 
 class Controllers {
     public function view($view) {
