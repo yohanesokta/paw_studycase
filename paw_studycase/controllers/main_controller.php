0580e6576326ffbb63c0c427d81c8cf30144316d
Andri-24 <andrifirmansyah381@gmail.com>
2025-11-20 20:57:46 +0700

    Delete paw_studycase directory

--- paw_studycase/controllers/main_controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-class MainController extends Controllers {
-    public function index() {
-        $this->view("home");
-    }
-}
--- paw_studycase/lib/loader.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "lib/controllers.php";
-require_once "lib/middleware.php";
-require_once "config/koneksi.php";
-require_once "controllers/main_controller.php";
-require_once "controllers/authController.php";
-require_once "controllers/adminController.php";
