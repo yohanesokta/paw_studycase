8c7b7fa6ac1af3be12e1fb0bfdbe4f2c6d856d27
suryamaulana98 <suryamaulana757@gmail.com>
2025-11-21 00:58:21 +0700

    add file lib controller

--- lib/controllers.php
+++ lib/controllers.php
@@ -1,15 +1,21 @@
 <?php
-function URL($url) {
-    return $_ENV['proyekname'].$url;
+function URL($url)
+{
+    return $_ENV['proyekname'] . $url;
 }
-function Redirect($url) {
-    header("Location: ".URL($url));
+
+function Redirect($url)
+{
+    header("Location: " . URL($url));
     exit();
 }
 
+class Controllers
+{
 
-class Controllers {
-    public function view($view) {
+    public function view($view, $data = [])
+    {
+        extract($data); // ini kirim variabel ke view
         require "views/$view.php";
     }
 }
