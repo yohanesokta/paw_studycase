89efd9c73ae99f3f03bf5e597d6e84f42bbba703
Anasasemjaran <ttrapang41@gmail.com>
2025-11-24 09:40:43 +0700

    Update master_harga.php

--- views/admin/master_harga.php
+++ views/admin/master_harga.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Master Harga & Jenis Layanan - Admin</title>
-   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= URL('/public/css/home.css') ?>">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body class="bg-light">
 
