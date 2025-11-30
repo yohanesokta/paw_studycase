e9d49fd4fac5f928594f19801d5d3093a2b6547f
suryamaulana98 <suryamaulana757@gmail.com>
2025-11-30 18:28:18 +0700

    menambahkan informasi orderbelum selesai, dan memperbaiki count card di dashboard

--- views/admin/components/footer.php
+++ views/admin/components/footer.php
@@ -1,3 +1,6 @@
-</div> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
+
 </html>
