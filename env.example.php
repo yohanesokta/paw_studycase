eaac72740d1e51b0076021ddff2014a7aca91339
Andri-24 <andrifirmansyah@gmail.com>
2025-11-30 21:02:16 +0700

    update laporan pelanggan v7

--- env.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$_ENV ['google_client_id'] = "543938403209-v26o1i3oav121amgt4njl1rpuog17m0i.apps.googleusercontent.com";
-$_ENV ["google_client_secret"] = "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";
-$_ENV ['rederict_url'] = 'http://localhost/paw_studycase/google/callback';
-$_ENV ["MYSQL_DATABASE"] = "laundry-online";
-?>
-
